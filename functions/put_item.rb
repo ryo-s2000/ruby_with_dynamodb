c3967602e657f14959ca86baf4e1891f4a22b6a0
Ryo Suzuki <ryo.s12330@gmail.com>
2022-10-30 00:53:08 +0900

    put item

--- functions/put_item.rb
+++ functions/put_item.rb
@@ -1,3 +1,32 @@
+require 'aws-sdk-dynamodb'
+
 def put_item
-    'put_item'
+  call_main_func
 end
+
+private
+
+def call_main_func
+  region = 'ap-northeast-1'
+  dynamodb_client = Aws::DynamoDB::Client.new(region: region)
+
+  table_name = 'browsing_history'
+
+  uid = 'uid-28583'
+
+  iid = "iid-33222"
+  image = ""
+
+  item = {
+    table_name: table_name,
+    item: {
+        "uid" => uid,
+        "iid" => iid,
+        "image" => image,
+    },
+  }
+
+  dynamodb_client.put_item(item)
+end
+
+put_item if $PROGRAM_NAME == __FILE__
